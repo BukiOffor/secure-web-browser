1189d39ecbaf13cff05493de3a87c8412f26dc73
BukiOffor <ebuka2264@yahoo.com>
2025-06-25 08:08:08 +0100

    feat: build release

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,47 +1,76 @@
-use tauri::{menu::Menu, window::Window, Manager, WebviewWindowBuilder};
+#![allow(unused_imports)]
 use tauri::menu::MenuBuilder;
+use tauri::{Emitter, Manager};
+use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
+pub mod utils;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
+    tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
 
-      let args: Vec<String> = std::env::args().collect();
-      println!("arguments: {:?}", args);
-      let is_kiosk = args.iter().any(|arg| arg == "kiosk");
-      println!("Running app in kiosk mode set to : {}", is_kiosk);
+            let args: Vec<String> = std::env::args().collect();
+            println!("arguments: {:?}", args);
+            let is_kiosk = args.iter().any(|arg| arg == "kiosk");
+            println!("Running app in kiosk mode set to : {}", is_kiosk);
 
-      let window = app.get_webview_window("main").unwrap();
-      window.set_fullscreen(is_kiosk.clone())?;
-      window.set_decorations(!is_kiosk.clone())?;
-      window.set_always_on_top(is_kiosk.clone())?;
-      window.set_resizable(!is_kiosk.clone())?;
-      //window.hide_menu()?;
-      //window.remove_menu()?;
-      let manager = app.handle();
-      let menu = MenuBuilder::new(manager);
-      //window.set_menu(Menu::new(manager)?)?;
+            let window = app.get_webview_window("main").unwrap();
+            window.set_fullscreen(is_kiosk.clone())?;
+            window.set_decorations(!is_kiosk.clone())?;
+            window.set_always_on_top(is_kiosk.clone())?;
+            window.set_resizable(!is_kiosk.clone())?;
+            //window.hide_menu()?;
+            //window.remove_menu()?;
+            let manager = app.handle();
+            let menu = MenuBuilder::new(manager);
+            //window.set_menu(Menu::new(manager)?)?;
+            let menu = menu.build()?;
+            window.set_menu(menu)?;
 
-      let menu = menu.build()?;
-      window.set_menu(menu)?;
- 
+            let ctrl_k_shortcut = Shortcut::new(Some(Modifiers::CONTROL), Code::KeyK);
+            let cltr_alt_delete_shortcut =
+                Shortcut::new(Some(Modifiers::CONTROL | Modifiers::ALT), Code::Delete);
+            let minimized_shortcut = Shortcut::new(Some(Modifiers::SUPER), Code::KeyD);
+            app.handle().plugin(
+                tauri_plugin_global_shortcut::Builder::new()
+                    .with_handler(move |app, shortcut, event| {
+                        utils::build_bindings(
+                            app,
+                            shortcut,
+                            &event,
+                            &ctrl_k_shortcut,
+                            &cltr_alt_delete_shortcut,
+                            &minimized_shortcut,
+                        );
+                    })
+                    .build(),
+            )?;
+            app.global_shortcut().register(ctrl_k_shortcut)?;
+            app.global_shortcut().register(cltr_alt_delete_shortcut)?;
+            app.global_shortcut().register(minimized_shortcut)?;
 
-      Ok(())
-    })
-     .on_window_event({
+            // Check if running in a guest machine on windows    
+            if utils::is_virtual_machine() || utils::is_running_in_rdp() {
+                println!("Running in a guest machine, exiting...");
+                app.handle().exit(0);
+            }
+            
+            Ok(())
+        })
+        .on_window_event({
             move |_window, event| {
                 if let tauri::WindowEvent::CloseRequested { api, .. } = event {
-                    api.prevent_close(); 
+                    api.prevent_close();
                 }
             }
         })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
---
+++ src-tauri/src/utils/mod.rs
@@ -0,0 +1,171 @@
+use std::process::Command;
+
+use serde::Serialize;
+use tauri::{AppHandle, Emitter};
+use tauri_plugin_global_shortcut::{Code, Modifiers, Shortcut, ShortcutEvent, ShortcutState};
+use mac_address::get_mac_address;
+
+#[derive(Debug, Clone, Serialize, Default)]
+pub struct HostInfo {
+    pub os: String,
+    pub arch: String,
+    pub mac_address: Option<String>,
+    pub serial_number: Option<String>,
+    pub processor_id : Option<String>
+}
+
+
+pub fn build_bindings(
+    app: &AppHandle,
+    shortcut: &Shortcut,
+    event: &ShortcutEvent,
+    ctrl_k_shortcut: &Shortcut,
+    cltr_alt_delete_shortcut: &Shortcut,
+    minimized_shortcut: &Shortcut,
+) {
+    // Handle the global shortcuts
+    println!("{:?}", shortcut);
+    if shortcut == ctrl_k_shortcut {
+        match event.state() {
+            ShortcutState::Pressed => {
+                println!("Ctrl-K Pressed!");
+            }
+            ShortcutState::Released => {
+                println!("Ctrl-K Released!");
+                app.emit("show-password-prompt", ())
+                    .expect("Failed to emit show-password-prompt");
+                app.exit(0);
+            }
+        }
+    } else if shortcut == cltr_alt_delete_shortcut {
+        match event.state() {
+            ShortcutState::Pressed => {
+                println!("Ctrl+Alt+Delete Pressed!");
+            }
+            ShortcutState::Released => {
+                println!("Ctrl+Alt+Delete Released!");
+                app.emit("show-ctrl-alt-delete-prompt", ())
+                    .expect("Failed to emit show-ctrl-alt-delete-prompt");
+            }
+        }
+    } else if shortcut == minimized_shortcut {
+        match event.state() {
+            ShortcutState::Pressed => {
+                println!("Super+D Pressed!");
+            }
+            ShortcutState::Released => {
+                println!("Super+D Released!");
+            }
+        }
+    }
+}
+
+#[cfg(target_os = "windows")]
+pub fn is_virtual_machine() -> bool {
+    // let cpuid = raw_cpuid::CpuId::new();
+    //   if let Some(vf) = cpuid.get_vendor_info() {
+    //     let vendor = vf.as_str().to_lowercase();
+    //     return vendor.contains("vmware")
+    //         || vendor.contains("virtualbox")
+    //         || vendor.contains("qemu")
+    //         || vendor.contains("kvm")
+    //         || vendor.contains("microsoft hyper-v");
+    // }
+
+    // false
+    use inside_vm::inside_vm;
+    inside_vm() 
+}
+
+#[cfg(not(target_os = "windows"))]
+pub fn is_virtual_machine() -> bool {
+    false   
+}
+
+
+#[cfg(target_os = "windows")]
+pub fn is_running_in_rdp() -> bool {
+    use windows::Win32::UI::WindowsAndMessaging::GetSystemMetrics;
+    use windows::Win32::UI::WindowsAndMessaging::{SM_REMOTESESSION};
+
+    unsafe { GetSystemMetrics(SM_REMOTESESSION) != 0 }
+}
+
+#[cfg(not(target_os = "windows"))]
+pub fn is_running_in_rdp() -> bool {
+    false
+}
+
+pub fn get_windows_serial() -> Option<String> {
+    let output = Command::new("wmic")
+        .args(&["bios", "get", "serialnumber"])
+        .output()
+        .ok()?;
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    for line in stdout.lines() {
+        if line.trim().is_empty() || line.contains("SerialNumber") {
+            continue;
+        }
+        return Some(line.trim().to_string());
+    }
+
+    None
+}
+
+pub fn get_macos_serial() -> Option<String> {
+    let output = Command::new("system_profiler")
+        .args(["SPHardwareDataType"])
+        .output()
+        .ok()?;
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    for line in stdout.lines() {
+        if line.trim_start().starts_with("Serial Number") {
+            return Some(line.split(':').nth(1)?.trim().to_string());
+        }
+    }
+
+    None
+}
+
+pub fn get_linux_serial() -> Option<String> {
+    let output = Command::new("cat")
+        .args(&["/sys/class/dmi/id/product_serial"])
+        .output()
+        .ok()?;
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    for line in stdout.lines() {
+        if line.trim().is_empty() {
+            continue;
+        }
+        return Some(line.trim().to_string());
+    }
+
+    None
+}
+
+pub fn get_host_info() -> HostInfo {
+    let mut host_info = HostInfo::default();
+    let mac_address = get_mac_address()
+        .ok()
+        .and_then(|mac| mac.map(|m| m.to_string()));
+    host_info.mac_address = mac_address;
+    host_info.processor_id = Some(std::process::id().to_string());
+    host_info.os = std::env::consts::OS.to_string();
+    host_info.arch = std::env::consts::ARCH.to_string();
+    #[cfg(target_os = "windows")]
+    {
+        host_info.serial_number = get_windows_serial();
+    }
+    #[cfg(target_os = "macos")]
+    {
+        host_info.serial_number = get_macos_serial();
+    }
+    #[cfg(target_os = "linux")]
+    {
+        host_info.serial_number = get_linux_serial();
+    }
+    host_info
+}
